ef509bc8cb435baf7e6d9fd56474fed1b745b354
Kirill Fuchs <kfuchs@fuzzproductions.com>
2016-07-28 15:01:58 -0400

    setup test command

--- setup.py
+++ setup.py
@@ -1,36 +1,63 @@
+#!/usr/bin/env python
+from __future__ import with_statement
+
+import sys
+
 try:
-    from setuptools import setup
+    from setuptools import setup, Extension, Command
 except ImportError:
-    from ez_setup import use_setuptools
+    from distutils.core import setup, Extension, Command
 
-    use_setuptools()
-    from setuptools import setup
+IS_PYPY = hasattr(sys, 'pypy_translation_info')
+VERSION = '0.1'
+DESCRIPTION = "A not so magical repository pattern with support for Django, SQLAlchemy (coming soon). And some pre-built middleware to make it ready out of the box."
 
-setup(
-        name="magicbox",
-        version="0.1",
-        description="A not so magical repository pattern with support for Django, SQLAlchemy (coming soon). And some pre-built middleware to make it ready out of the box.",
-        long_description="A not so magical repository pattern with support for Django, SQLAlchemy (coming soon). And some pre-built middleware to make it ready out of the box.",
-        author="Kirill Fuchs",
-        author_email="kfuchs@fuzzproductions.com",
-        license="MIT License",
-        packages=[
-            "magicbox",
-        ],
-        include_package_data=True,
-        tests_require=[
-            "nose",
-            "coverage",
-        ],
-        zip_safe=False,
-        test_suite="tests.runtests.start",
-        classifiers=[
-            "Operating System :: OS Independent",
-            "Development Status :: 2 - Pre-Alpha",
-            "Environment :: Web Environment",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: MIT License",
-            "Programming Language :: Python :: 3",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-        ]
-)
+class TestCommand(Command):
+    nose_args = []
+    user_options = []
+
+    # If we run test just pass all arguments over to nosetest
+    if sys.argv[1] == 'test' and len(sys.argv) > 2:
+        nose_args = sys.argv[2:]
+        sys.argv = sys.argv[:2]
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import nose
+        nose.run_exit(
+                argv=["nosetests"] + self.nose_args
+        )
+
+
+def run_setup():
+    cmdclass = dict(test=TestCommand)
+    kw = dict(cmdclass=cmdclass)
+
+    setup(
+            name="magicbox",
+            version=VERSION,
+            description=DESCRIPTION,
+            long_description=DESCRIPTION,
+            classifiers=[
+                "Operating System :: OS Independent",
+                "Development Status :: 2 - Pre-Alpha",
+                "Environment :: Web Environment",
+                "Intended Audience :: Developers",
+                "License :: OSI Approved :: MIT License",
+                "Programming Language :: Python :: 3",
+                "Topic :: Software Development :: Libraries :: Python Modules",
+            ],
+            author="Kirill Fuchs",
+            author_email="kfuchs@fuzzproductions.com",
+            license="MIT License",
+            packages=['magicbox', 'tests'],
+            platforms=['any'],
+            **kw)
+
+
+run_setup()
